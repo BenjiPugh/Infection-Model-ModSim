2ae3a2ea283a9caff396bec1e76acad1db3d6398
AntoinetteTan <antoinette.tan.02@gmail.com>
2020-10-04 11:35:22 -0400

    Add files via upload

--- end_check.m
+++ end_check.m
@@ -4,7 +4,7 @@ w=-1;
 for j = 2: length(I)
     
     if ((I(1,j)-I(1,(j-1))<= 0) & (I(1,j)-I(1,(j-1))>= -1)) & (I(1, j-1) < max(I))
-        w = j;
+        w = j+1;
         break
         
     end
